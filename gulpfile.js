4196ec2815c27ceaef6a5a63fc263ef40f992ba4
Valeriy Nefyodov <valeriy.nefyodov@gmail.com>
2018-01-15 16:44:49 +0300

    Styles build modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,46 @@
-const gulp = require('gulp');
-const concat  = require('gulp-concat');
-const cssUrlAdj = require('gulp-css-url-adjuster');
-const del = require('del');
+const path        = require('path');
+const del         = require('del');
 const browserSync = require('browser-sync').create();
-const webpack = require('webpack');
-const gutil = require('gulp-util');
+const webpack     = require('webpack');
+const history     = require('connect-history-api-fallback');
+
+const gulp       = require('gulp');
+const concat     = require('gulp-concat-multi');
+const cssUrlAdj  = require('gulp-css-url-adjuster');
+const sourcemaps = require('gulp-sourcemaps');
+const gutil      = require('gulp-util');
 
 gulp.task('clean', function() {
     return del('public/**/');
 });
 
-gulp.task('styles', function(callback) {
-    gulp.src('src/css/BrandName/*.css')
+gulp.task('styles', function() {
+    return concat({
+        'google.css': 'src/css/Google/*.css',
+        'brandname.css': 'src/css/BrandName/*.css',
+        'portfolio.css': 'src/css/Portfolio/*.css',
+        'images.css': 'src/css/Images/*.css',
+    })
+        .pipe(sourcemaps.init())
         .pipe(cssUrlAdj({
             replace: ['../../', '../']
         }))
-        .pipe(concat('brandname.css'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/css/'));
-
-    gulp.src('src/css/Google/*.css')
-        .pipe(cssUrlAdj({
-            replace: ['../../', '../']
-        }))
-        .pipe(concat('google.css'))
-        .pipe(gulp.dest('public/css/'));
-
-    gulp.src('src/css/Images/*.css')
-        .pipe(cssUrlAdj({
-            replace: ['../../', '../']
-        }))
-        .pipe(concat('images.css'))
-        .pipe(gulp.dest('public/css/'));
-
-    gulp.src('src/css/Portfolio/*.css')
-        .pipe(cssUrlAdj({
-            replace: ['../../', '../']
-        }))
-        .pipe(concat('portfolio.css'))
-        .pipe(gulp.dest('public/css/'));
-
-    callback();
 });
 
-gulp.task('assets', function (callback) {
-    gulp.src(['src/{img,fonts}/**/*.*', 'src/favicon.ico', 'src/index.html'])
+gulp.task('assets', function () {
+    return gulp.src(['src/{img,fonts}/**/*.*', 'src/favicon.ico', 'src/index.html'])
         .pipe(gulp.dest(function(file) {
             file.base = 'src/';
             return 'public/'
         }));
-
-    callback();
 });
 
 gulp.task('webpack', function(callback) {
     return webpack(require('./webpack.config.js'), function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
+        if(err)
+            throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString());
         callback();
     });
@@ -67,11 +53,20 @@ gulp.task('watch', function() {
 
 gulp.task('serve', function(){
     browserSync.init({
-        server: './public/'
+        server: {
+            baseDir: './public/',
+            middleware: history({})
+        }
     });
 
-    gulp.watch('public/**/*.*').on('change', browserSync.reload);
+    browserSync.watch('public/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('build', gulp.series('clean', 'assets', gulp.parallel('styles', 'webpack')));
 gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
+
+gulp.task('styles:test', gulp.series('clean', 'stylesDev'));
+
+// let relDirName = file.dirname.split(path.sep);
+// filename = relDirName[relDirName.length - 1].toLowerCase();
+// gutil.log(filename);
